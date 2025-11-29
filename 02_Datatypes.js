ce53c05858f57dbd04745342ee9214377e6dbd43
Priyanshu Gupta <pg7297602@gmail.com>
2025-11-29 15:48:02 +0530

    Covered Datatypes

---
+++ 02_Datatypes.js
@@ -0,0 +1,36 @@
+"use strict" // treat my JS code as a newer version of javascript code
+
+//alert(3 + 3) // we are using nodejs not browser, here the syntax is a little bit different.
+
+console.log(3
+    +
+    3
+) // not considered as a good pracrice, code readability should bo high 
+
+console.log("Priyanshu") // code readabilty is high here, wee should prefer this rather.
+
+// throughout our webdev journey, two kind of doucmentation that we are always going to refer is mdn and tc39.
+
+let name = "Priyanshu"
+let age = 20
+let isLoggedIn =  true
+
+/* Datatypes:-
+// Primitive datatypes
+number => range: 2 to the power 53
+bigint => for larger values
+string => ""
+boolean => true/false
+null => standalone value
+undefined => type of a variable which has just been created but no value is assigned to it
+symbol => for uniqueness
+
+// Non-Primitive datatypes
+object
+ 
+*/
+
+console.log(typeof null) // type of null is object 
+console.log(typeof undefined) // but type of undefined is undefined
+
+
