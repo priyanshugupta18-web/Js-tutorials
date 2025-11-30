95c629b308560075387427172e3e6fd7b26d8df8
Priyanshu Gupta <pg7297602@gmail.com>
2025-11-30 22:40:17 +0530

    Added operators to the previous topic

--- 01_Basics/03_Datatypes_conversion_confusion.js
+++ 01_Basics/03_Datatypes_conversion_confusion.js
@@ -53,6 +53,60 @@
 // console.log(typeof stingNumber) // output: String
 // console.log(stringNumber) // output: 18 
 
+//=============operations=============//
 
+// simple example of an operation:-
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
 
+// Arithematic operations:-
+// console.log(2+2) // 2 plus 2
+// console.log(2-2) // 2 minus 2
+// console.log(2*2) // 2 multiplied by 2
+// console.log(2**3) // 2 to the power 3 ('**' used for power)
+// console.log(2/5) // 2 divided by 5
+// console.log(2 % 5) // 2 modulus 5(modulus operator is used for finding remainder.)
 
+// Note: In the above case the numbers over which the operations are being performed are called operands and the signs which are which are symbolising a particular operation being performed are called Operators.
+
+// let str1 = "Hello, "
+// let str2 = "Priyanshu Gupta"
+
+// let str3 = str1 + str2
+// console.log(str3)
+// In the above, We saw simple concatenation of two strings.
+
+// Performing operations between operands of different datatypes:-
+
+// console.log(1 + "2") // output: firstly 1 will be converted into string and then they will concatinate and result will be 12.
+
+// console.log("1" + 2) // output; firstly 2 will be converted to the string and then they will concatinate and result will be 12.
+
+// console.log("1" + 2 + 2) // output: Here, since 1 is a string which is comming at the first position that's why both the 2 will be converted to string and then they will concatinate and the result will be 122.
+
+// console.log(1 + 2 + "2") // Here, since 2 is a string which is comming at the last position that's why 1 and 2 will be arithematically added to each other and then the result be converted to string which will further contcatinate with 2 to give the final output: 32.
+
+// Note: See operator precedence is one thing but we should always remember as a professional that are codes are going to be reviewed multiple times, so we should keep it clean and readable for other by applying brackets.
+
+// Operations on some other Datatypes:-
+
+// console.log(+true) // output: 1
+// console.log(+false) // output: 0
+
+// console.log(+"") // output: 0
+// console.log(+"Priyanshu") // output: 1
+
+// Pre-increment and post-increment operators:-
+// Pre-increment Operator:-
+// It basically increases the value of a constant or a variable by 1 before it's utilisation/ usage.
+// for example:-
+//let num = 5
+//console.log(++num) // output: 6 (first increased by 1, then printed)
+
+//Post-increament operator:-
+//It basically utilises the constant or variable and then increases its value by 1.
+// for example:-
+//let num = 5
+//console.log(num++) // output: 5 (first utilised and then increased by 1)
+//console.log(num) // output: 6 (shows that the value was increased by 1 after printing).
