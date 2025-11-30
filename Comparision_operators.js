351832ecb7ed2007ea868a78fac926c99ca247df
Priyanshu Gupta <priyanshuguptawebdev@gmail.com>
2025-12-01 01:23:50 +0530

    done with 04_Comparision_operators

---
+++ Comparision_operators.js
@@ -0,0 +1,29 @@
+// Relational Operators:-
+
+// console.log(2 > 1) // greater than
+// console.log(2 >= 1) // greater than equal to
+// console.log(2 < 1) // less than
+// console.log(2 <= 1) // less than equal to
+// console.log(2 == 1) // equal to 
+// console.log(2 === 1) // equal to(strict checking)
+// console.log(2 != 1) // not equal to
+
+// Note:- a statement including a relational operator will always return a boolean output (true/False)
+
+// strict checking means like in case of '==' if we compare 2 and '2' it will return true as '2' will be converted to number over here just because there is no strict checking, but in case of strict checking (i.e. '===') it will return false.
+
+//Comparision between different data types:-
+
+//console.log(null > 0) // output: false
+// explaination: Here, in this case null is converted to NaN and comparing null with NaN will always return false
+
+//console.log(null == 0) // output: false
+// explaination: Here, in this case null is converted to NaN and comparing null with NaN will always return false
+
+//console.log(null >= 0) // output: true
+// explaination: since the equal to opeartor and the greater than and less operator than behaves differently that's why here in this case null is converted to 0 and hence the expression return true.
+
+// Some comparisions (forget about logic just have a look on it and that's it.):-
+// console.log(undefined == 0) output: false
+// console.log(undefined > 0) output: false
+// console.log(undefined < 0) output: false
